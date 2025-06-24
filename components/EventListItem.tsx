57b02979445bb7a1090fb28bff2f9cc054ee11e8
Suleiman Odetoro <odetoro75@gmail.com>
2025-06-24 09:51:18 +0100

    refactor: format event dates using dayjs

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -2,16 +2,16 @@ import Entypo from '@expo/vector-icons/Entypo';
 import Feather from '@expo/vector-icons/Feather';
 import { View, Text, Image } from 'react-native';
 
-
+import dayjs from 'dayjs'
 const EventListItem = ({event}) => {
   return (
-    <View className="gap-3 p-3">
+    <View className="gap-3 m-3 pb-3 border-b-2 border-gray-100 ">
       {/* Card Top Section: Texts + Image */}
       <View className="flex-row">
         {/* Event Details */}
         <View className="flex-1 gap-2">
           <Text className="text-lg font-semibold uppercase text-amber-800">
-            {event.datetime}
+            {dayjs(event.datetime).format('ddd, D, MMM')}· {dayjs(event.datetime).format('h:mm A')}
           </Text>
           <Text className="text-xl font-bold" numberOfLines={2}>
             {event.title}
