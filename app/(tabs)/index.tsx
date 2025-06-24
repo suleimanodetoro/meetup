ec80ee5c86f4846e099d41d8146891b71725fe81
Suleiman Odetoro <odetoro75@gmail.com>
2025-06-24 09:36:42 +0100

    feat: render events from JSON data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,23 @@
 import { Stack } from 'expo-router';
 import Entypo from '@expo/vector-icons/Entypo';
 import Feather from '@expo/vector-icons/Feather';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, FlatList } from 'react-native';
 import EventListItem from '~/components/EventListItem';
 
+import events from '~/assets/events.json';
 
 export default function Home() {
   return (
     <>
       {/* Screen header */}
       <Stack.Screen options={{ title: 'Events' }} />
-      <EventListItem />
-      <EventListItem />
-
-      
+      {/* List of event items */}
+      <FlatList
+        data={events}
+        className='bg-white'
+        renderItem={({ item }) => <EventListItem event={item} />}
+        keyExtractor={(item) => item.id}
+      />
     </>
   );
 }
